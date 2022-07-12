3daf0ba0f664366ff970f929ba1bc99090b118cd
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2022-07-12 20:43:23 +0930

    Added WIP setup script

---
+++ build.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const setupFile = fs.readFileSync('src/setup.sh');
+const baseConfig = fs.readFileSync('src/example-config/base.js');
+const reactConfig = fs.readFileSync('src/example-config/react.js');
+const prettierConfig = fs.readFileSync('src/example-config/prettier.js');
+
+const setupFileString = setupFile.toString();
+const substitutedFile = setupFileString
+  .replace('%% BASE_CONFIG %%', baseConfig.toString())
+  .replace('%% REACT_CONFIG %%', reactConfig.toString())
+  .replace('%% PRETTIER_CONFIG %%', prettierConfig.toString());
+
+fs.writeFileSync('bin/setup.sh', substitutedFile);
---
+++ src/setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Get the directory of the script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+read -p "Do you need react eslint rules? [y/n]: " setup_react_rules;
+
+echo "Installing core linting dependencies"
+npm i --save-dev @trivago/prettier-plugin-sort-imports @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-prettier eslint-plugin-prettier prettier
+
+if [ "$setup_react_rules" == "y" ]; then
+    echo "Installing react linting dependencies"
+    npm i --save-dev eslint-plugin-react eslint-plugin-react-hooks
+fi
+
+base_config="%% BASE_CONFIG %%"
+react_config="%% REACT_CONFIG %%"
+prettier_config="%% PRETTIER_CONFIG %%"
+
+# Copy prettier config across
+echo "Copying prettier config"
+echo "$prettier_config" > .prettierrc.js
+
+# Copy the correct eslint config
+echo "Copying eslint config"
+if [ "$setup_react_rules" == "y" ]; then
+    echo "$react_config" > .eslintrc.js
+else
+    echo "$base_config" > .eslintrc.js
+fi
