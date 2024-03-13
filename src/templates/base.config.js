297608375da02c429aff7257eda00d9a2c61e53f
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2024-03-13 22:51:25 +1030

    task: update templates with spread of shared config

--- src/templates/base.config.js
+++ src/templates/base.config.js
@@ -1,7 +1,7 @@
 const config = require('@dtdot/eslint-config');
 
 module.exports = [
-  config.eslint.configs.recommended,
+  ...config.eslint.configs.recommended,
   {
     ignores: ['node_modules', 'dist', 'eslint.config.js', 'prettier.config.js'],
     files: ['**/*.js', '**/*.ts'],
